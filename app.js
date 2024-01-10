66338a9669b0e0517c979bf2f4cf8b6bd19a4a01
Imad Hamani <90478406+nerovanity@users.noreply.github.com>
2024-01-10 16:16:19 +0100

    Environment Variables

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const usersRouter = require('./routers/usersRouters');
 
 const app = express();
 //Middleware:middle of the request and the response
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
