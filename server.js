f614e213723070ddc8082ce91f5ab9b1b34f9196
Imad Hamani <90478406+nerovanity@users.noreply.github.com>
2024-01-13 21:15:21 +0100

    connecting db and deleting config.env

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({path: './config.env'});
+
+const db = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose.connect(db, {}).then(() =>{
+    console.log("database is connected");
+})
 
 const app = require('./app');
 
